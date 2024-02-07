be8127fc9d4ad5fe1be8bdbbe38ab668b04e9737
akiramenowaruiotoko <sawaking@hotmail.co.jp>
2024-02-07 21:23:11 +0900

    Polymorphism create Cat class

---
+++ PolymorphismSamples/PolymorphismSamples/Cat.cs
@@ -0,0 +1,19 @@
+﻿using System.Drawing;
+using PolymorphismSamples.Properties;
+
+namespace PolymorphismSamples
+{
+    internal class Cat : Animal // inherit from animal class
+    {
+        public string Nose { get; set; } = "round"; // nose shape
+        public override Image Sing()
+        {
+            return Resources.CatCookieSing;
+        }
+
+        public override Image Reset()
+        {
+            return Resources.CatCookie;
+        }
+    }
+}
